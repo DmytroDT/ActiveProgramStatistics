f75ff4c5921b665f99c1c571e89d80bd1031759c
DmytroDT <Dohgca@gmail.com>
2019-07-21 13:38:40 +0300

    Add project files.

---
+++ ActiveProgramStatistics/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ActiveProgramStatistics
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //placeholder class
+        class StatWatcher
+        {
+            //declare a static list of all class instances
+
+            static List<StatWatcher> StatWatcherList = new List<StatWatcher>();
+
+
+            //creating visual component representing foreground opened program 
+
+            public StatWatcher(Panel ExternalPanel, String StringName = "Placeholder")
+            {
+                Panel panel = new Panel();
+                panel.Size = new Size(100, 406);
+                panel.BorderStyle = BorderStyle.Fixed3D;
+
+                //creating lable that will get data from ProgramStatisticsGetter Class
+                Label lb = new Label();
+                lb.Text = StringName;
+                panel.Controls.Add(lb);
+
+                //adding this component to external panel
+                ExternalPanel.Controls.Add(panel);
+                panel.Location = new Point(0,0);
+
+
+                StatWatcherList.Add(this);
+            }
+
+            
+
+        }
+    }
+}
---
+++ ActiveProgramStatistics/ProgramStatiscisGetter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ActiveProgramStatistics
+{
+    class ProgramStatiscisGetter
+    {
+        //Importing external dll to monitor windows foreground processes
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        public static extern UInt32 GetWindowThreadProcessId(IntPtr hwnd, ref Int32 pid);
+
+
+
+        IntPtr h = GetForegroundWindow();
+        int pid = 0;
+
+        // creating variable with properties for getting the title of an active window
+
+        string SrtingName;
+        string MainWindowTitle
+        {
+            get { return SrtingName; }
+            set { GetWindowThreadProcessId(h, ref pid); Process process = Process.GetProcessById(pid); SrtingName = process.MainWindowTitle; }
+        }
+
+    
+      
+
+        
+    }
+}
